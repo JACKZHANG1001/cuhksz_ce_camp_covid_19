fc0f64c7b3b9b7f027be40483b9c9aa410b96a87
Baozhe <119010421@link.cuhk.edu.cn>
2020-06-04 15:58:58 +0800

    r parameter about the S-E and S-I

--- test_model.m
+++ test_model.m
@@ -8,7 +8,7 @@ I = 1;
 R = 0;
 E = 0;
 S = N - I;
-
+r1 = 3;
 a = 1/8; % p for E
 B1 = 0.15747;
 B2 = 0.78735 - 0.5;
@@ -20,11 +20,11 @@ T = 1:100;
 
 for t = 1:length(T) - 1
     if t >= 1 && t <= 10
-        r = 15;
+        r2 = 15;
     elseif t >= 11 && t <= 80
-        r = 3;
+        r2 = 3;
     else
-        r = 10;
+        r2 = 10;
     end
     
     if t >= 1 && t <= 4
@@ -39,8 +39,8 @@ for t = 1:length(T) - 1
     
     y = y1 + y2; % gamma
     
-    S(t+1) = S(t) - r*B1*I(t)*S(t)/N - r*B2*E(t)*S(t)/N;
-    E(t+1) = E(t) + r*B1*I(t)*S(t)/N + r*B2*E(t)*S(t)/N - a*E(t);
+    S(t+1) = S(t) - r1*B1*I(t)*S(t)/N - r2*B2*E(t)*S(t)/N;
+    E(t+1) = E(t) + r1*B1*I(t)*S(t)/N + r2*B2*E(t)*S(t)/N - a*E(t);
     I(t+1) = I(t) + a*E(t) - y*I(t);
     R(t+1) = R(t) + y*I(t);
     new_infected(t+1) = I(t+1) - I(t);
@@ -49,7 +49,6 @@ end
 plot(T, S, T, E, T, I, T, R, T, new_infected, 'linewidth', 3);
 
 grid on
-
 
 xlabel('date');
 ylabel('number');
